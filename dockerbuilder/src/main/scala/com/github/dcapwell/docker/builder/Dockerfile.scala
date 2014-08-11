1a6dcfd274804e611ef840f3e4256a91c31456f4
David Capwell <dcapwell@davidcapwellmbp.attlocal.net>
2014-08-10 22:02:52 -0700

    format

--- dockerbuilder/src/main/scala/com/github/dcapwell/docker/builder/Dockerfile.scala
+++ dockerbuilder/src/main/scala/com/github/dcapwell/docker/builder/Dockerfile.scala
@@ -2,9 +2,12 @@ package com.github.dcapwell.docker.builder
 
 import com.github.dcapwell.docker.builder.lang.Trait
 
+import scala.annotation.tailrec
+
 object Dockerfile {
 
   def generate(traits: List[Trait]): List[String] = {
+    @tailrec
     def loop(current: Trait, rest: List[Trait], accum: List[String]): List[String] = {
       val fromStr = current.from.map(_.toString).toList
 
@@ -13,6 +16,8 @@ object Dockerfile {
       if(rest.isEmpty) accum ++ fromStr ++ restStr
       else loop(rest.head, rest.tail, accum ++ fromStr ++ restStr)
     }
-    loop(traits.head, traits.tail, Nil)
+
+    if(traits.isEmpty) Nil
+    else loop(traits.head, traits.tail, Nil)
   }
 }
--- dockerbuilder/src/test/scala/com/github/dcapwell/docker/builder/DockerfileTest.scala
+++ dockerbuilder/src/test/scala/com/github/dcapwell/docker/builder/DockerfileTest.scala
@@ -3,11 +3,19 @@ package com.github.dcapwell.docker.builder
 import com.github.dcapwell.docker.builder.lang.{Lexer, Trait}
 
 class DockerfileTest extends Base {
-  "centos with java" in {
-    val centos = Trait.unapply(Lexer.lex(source("traits/centos.docker")).get).get
-    val java = Trait.unapply(Lexer.lex(source("traits/openjdk-7.docker")).get).get
+  val centos = Trait.unapply(Lexer.lex(source("traits/centos.docker")).get).get
+  val java = Trait.unapply(Lexer.lex(source("traits/openjdk-7.docker")).get).get
 
+  "centos with java" in {
     val content = Dockerfile.generate(List(centos, java))
     println(s"Content\n${content.mkString("\n")}")
+  }
+
+  "nil" in {
+    Dockerfile.generate(Nil) shouldBe Nil
+  }
+
+  "centos" in {
+    Dockerfile.generate(List(centos)) shouldBe List("FROM centos:centos6")
   }
 }
